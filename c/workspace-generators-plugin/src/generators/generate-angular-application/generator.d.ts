4500c5ea12fde72a099b6b1ad1e3538bf9f0fdfe
niarosrojas <nicolas.aros@gmail.com>
2025-03-23 17:48:52 +0100

    rearrange folders

--- c/workspace-generators-plugin/src/generators/generate-angular-application/generator.d.ts
+++
@@ -1,4 +0,0 @@
-import { Tree } from '@nx/devkit';
-import { GenerateAngularApplicationGeneratorSchema } from './schema';
-export declare function generateAngularApplicationGenerator(tree: Tree, options: GenerateAngularApplicationGeneratorSchema): Promise<void>;
-export default generateAngularApplicationGenerator;
--- c/workspace-generators-plugin/src/generators/generate-angular-library/generator.d.ts
+++
@@ -1,5 +0,0 @@
-import { Tree } from '@nx/devkit';
-import { GenerateAngularLibraryGeneratorSchema } from './schema';
-export declare function generateAngularLibraryGenerator(tree: Tree, // The file system tree that allows you to manipulate files
-options: GenerateAngularLibraryGeneratorSchema): Promise<void>;
-export default generateAngularLibraryGenerator;
